cbfcdbf42fc854788bd3ac059baf24993579c8fc
Vaibhav Singh <vaibhavsingh2633@gmail.com>
2024-03-22 16:25:26 +0530

    sad

--- components/Cards/QuestionCards.tsx
+++ components/Cards/QuestionCards.tsx
@@ -20,7 +20,7 @@ const QuestionCards = ({ _id, title, tags, author, upvotes, views, answer, creat
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
 <div>
   <span className='subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden'>
- {getTimestamp(createdAt)}
+ {getTimestamp(createAt)}
   </span>
   <Link href={`/question/${_id}`}>
   <h3 className='sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1'>{title}</h3>
